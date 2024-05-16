539de69e3d23fe407eada49173f601f5605e7cc7
PaikMyeongGyu <qoraudrb123@gmail.com>
2024-05-16 21:33:14 +0900

    chapter 17 병렬 프로그래밍이랑 같이 볼것!

---
+++ src/main/java/org/src/chapter17/Main.java
@@ -0,0 +1,15 @@
+package org.src.chapter17;
+
+import java.util.concurrent.Flow.Publisher;
+
+public class Main {
+
+    public static void main(String[] args) { // 뉴욕에 새 Publisher를 만들고 TempSubscriber를 구독시킴
+        getTemperatures("New York").subscribe(new TempSubscriber());
+    }
+
+    private static Publisher<TempInfo> getTemperatures(String town) { // 구독한 Subscriber에게 TempSubscription을 전송하는 Publisher를 반환
+        return subscriber -> subscriber.onSubscribe(new TempSubscription(subscriber, town));
+    }
+
+}
